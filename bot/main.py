ed5df4940910800671114109b71960862803d53c
PratikGarai <pratikgarai0208@gmail.com>
2022-10-26 16:06:07 +0530

    slash commands

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ import os
 
 import discord
 from db import MongoDbHandler
+from discord import app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
 from utils import get_message_details
 
@@ -9,23 +11,36 @@ load_dotenv()
 
 DB = None
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message: discord.Message):
-        message_details = get_message_details(message=message)
-        is_successful = DB.updateUserScore(message_details)
-        if is_successful : 
-            print("Updated user info")
-        else : 
-            print("User update failed")
-
-
 intents = discord.Intents.default()
 intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    print("Bot Ready!")
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+    message_details = get_message_details(message=message)
+    is_successful = DB.updateUserScore(message_details)
+    if is_successful:
+        print("Updated user info")
+    else:
+        print("User update failed")
+
+@tree.command(name="sync_commands", description="Synchronise commands to server")
+async def sync_commands(interaction : discord.Interaction):
+    await tree.sync(guild=interaction.guild)
+    await interaction.response.send_message("Synced Commands")
+
+
+@tree.command(name="get_top", description="Get top offensive users in server")
+async def get_top(interaction : discord.Interaction):
+    await interaction.response.send_message("Hello I am bot")
 
 if __name__ == "__main__":
     DB = MongoDbHandler(os.environ["DB_URL"], os.environ["DB_PASSWORD"])
-    client = MyClient(intents=intents)
     client.run(os.environ["DISCORD_CLIENT_SECRET"])
